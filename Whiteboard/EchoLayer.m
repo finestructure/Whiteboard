de09a1129f2898ad20767796192485b994e5f0d4
Sven A. Schmidt <sas@abstracture.de>
2012-05-02 12:33:23 +0200

    Replaying remote events

--- Whiteboard/EchoLayer.m
+++ Whiteboard/EchoLayer.m
@@ -11,6 +11,8 @@
 #import "cocos2d.h"
 
 #import "Database.h"
+#import "LinePoint.h"
+#import "WBEvent.h"
 
 
 @implementation EchoLayer
@@ -53,7 +55,88 @@
 {
   if ([keyPath isEqualToString:@"rows"]) {
     NSLog(@"change: %@", change);
+    CouchQueryEnumerator *newRows = [change valueForKey:NSKeyValueChangeNewKey];
+    CouchQueryRow *row;
+    while ((row = newRows.nextRow)) {
+      WBEvent *event = [WBEvent modelForDocument:row.document];
+      NSLog(@"processing event %@", event.index);
+      [self processEvent:event];
+    }
   }
+}
+
+
+#pragma mark - Touch event handling
+
+
+- (CGPoint)getPoint:(WBEvent *)event {
+  return [[CCDirector sharedDirector] convertToGL:event.position];
+}
+
+
+- (void)processEvent:(WBEvent *)event {
+  switch ([event.phase unsignedIntegerValue]) {
+    case UITouchPhaseBegan:
+      [self touchBegan:event];
+      break;
+      
+    case UITouchPhaseMoved:
+      [self touchMoved:event];
+      break;
+      
+    case UITouchPhaseEnded:
+      [self touchEnded:event];
+      break;
+      
+    case UITouchPhaseCancelled:
+      [self touchCancelled:event];
+      break;
+      
+    default:
+      break;
+  }
+}
+
+
+- (BOOL)touchBegan:(WBEvent *)event {
+  CGPoint point = [self getPoint:event];
+  
+  [self.points removeAllObjects];
+  [self.velocities removeAllObjects];
+  
+  [self startNewLineFrom:point withSize:self.penWidth];
+  [self addPoint:point withSize:self.penWidth];
+  [self addPoint:point withSize:self.penWidth];
+  
+  return YES;
+}
+
+
+- (void)touchMoved:(WBEvent *)event {
+  CGPoint point = [self getPoint:event];
+  float eps = 1.5f;
+  if ([self.points count] > 0) {
+    float length = ccpLength(ccpSub([(LinePoint *)[self.points lastObject] pos], point));
+    
+    if (length < eps) {
+      return;
+    }
+  }
+  //TODO: vary size
+  [self addPoint:point withSize:self.penWidth];
+}
+
+
+- (void)touchEnded:(WBEvent *)event {
+  CGPoint point = [self getPoint:event];
+  //TODO: vary size
+  [self endLineAt:point withSize:self.penWidth];
+  [self draw];
+}
+
+
+- (void)touchCancelled:(WBEvent *)event {
+  [self draw];
 }
 
 
